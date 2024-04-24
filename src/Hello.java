976a0ae0619b20855e2bf284b6b4e1b4546fa4ae
SEYEON-PARK <fourleaf123@naver.com>
2024-04-24 13:09:17 +0900

    fifth

--- src/Hello.java
+++ src/Hello.java
@@ -1,8 +1,8 @@
 public class Hello {
 
-    public static void run() {
-      System.out.println("hello world");
-    }
-  
+  public static void run() {
+    System.out.println("hello world");
+    System.out.println("박세연");
   }
-  
+
+}
