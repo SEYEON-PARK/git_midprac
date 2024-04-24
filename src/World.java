357348d3fad801043052eb5f3aa4f35e3995f916
SEYEON-PARK <fourleaf123@naver.com>
2024-04-24 13:07:29 +0900

    fourth

--- src/World.java
+++ src/World.java
@@ -1,8 +1,8 @@
 public class World {
 
-    public static void run() {
-      System.out.println("안녕하세요");
-    }
-  
+  public static void run() {
+    System.out.println("안녕하세요");
+    System.out.println("박세연");
   }
-  
+
+}
